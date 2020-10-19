f2dea095d686589e50acf4636e24cf999218e4cb
kanevk <kamen.e.kanev@gmail.com>
2020-10-19 16:58:08 +0300

    Updates

---
+++ precache-manifest.49e66ca734569bedb8293a7eb58db30e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25c718b479e2c8c10c0b55eec216f08",
+    "url": "/re-poker/index.html"
+  },
+  {
+    "revision": "b978175776b5b021b2f2",
+    "url": "/re-poker/static/css/main.3057b34d.chunk.css"
+  },
+  {
+    "revision": "9c00b96246450fbee158",
+    "url": "/re-poker/static/js/2.0fdacd41.chunk.js"
+  },
+  {
+    "revision": "b978175776b5b021b2f2",
+    "url": "/re-poker/static/js/main.f3654bff.chunk.js"
+  },
+  {
+    "revision": "f055e1c3288b35d62115",
+    "url": "/re-poker/static/js/runtime-main.0664a4a9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17a4ca0bf3b2c18c5ddc2b3b748bec65.js"
+  "/re-poker/precache-manifest.49e66ca734569bedb8293a7eb58db30e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/re-poker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
